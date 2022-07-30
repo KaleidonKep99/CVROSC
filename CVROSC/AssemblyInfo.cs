646442ef4a19ef33bfd3f957abb851079b1b4092
KaleidonKep99 <kaleidonkep99@outlook.com>
2022-07-30 11:16:11 +0200

    no message

--- CVROSC/AssemblyInfo.cs
+++ CVROSC/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using CVROSC;
 using System.Reflection;
 
-[assembly: MelonInfo(typeof(CVROSCMod), "CVROSC", "1.4", "bribi#5940")]
+[assembly: MelonInfo(typeof(CVROSCMod), "CVROSC", "1.5", "bribi#5940")]
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
